6208dc693f437a1b4400f83202574b67a3417b0a
unknown <hbq>
2020-12-20 11:56:48 +0800

    ok

--- example/App.vue
+++ example/App.vue
@@ -1,32 +1,14 @@
 <template>
   <div>
-    <component :is="componentName" />
+    <Widget url="js/demo.component.js" />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import Widget from './components/Widget'
 export default {
-  data() {
-    return {
-      componentName: 'div'
-    }
-  },
-  methods: {
-    async loadDynamic() {
-      const res = await axios.get('js/demo.component.js')
-      eval(res.data)
-      const DemoComponent = window.commonComponent.default
-      return { name: DemoComponent.name || 'Demo', component: DemoComponent }
-    },
-    async dynamicComponentRender() {
-      const { name, component } = await this.loadDynamic()
-      this.$options.components[name] = component
-      this.componentName = name
-    }
-  },
-  async mounted() {
-    this.dynamicComponentRender()
+  components: {
+    Widget
   }
 }
 </script>
---
+++ example/components/Widget.vue
@@ -0,0 +1,41 @@
+<template>
+  <component :is="defaultComponent" v-show="defaultComponent !== 'div'" />
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'mPaaS-Widget',
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return { defaultComponent: 'div' }
+  },
+  created() {
+    this.dynamicComponentRender()
+  },
+  methods: {
+    execute(jsContent) {
+      eval(jsContent)
+    },
+    async loadDynamic() {
+      const res = await axios.get(this.url)
+      this.execute(res.data)
+      const component = window.commonComponent.default
+      return {
+        name: 'XDynamicComponent-' + (component.name || 'temp'),
+        component
+      }
+    },
+    async dynamicComponentRender() {
+      const { name, component } = await this.loadDynamic()
+      this.$options.components[name] = component
+      this.defaultComponent = name
+    }
+  }
+}
+</script>
